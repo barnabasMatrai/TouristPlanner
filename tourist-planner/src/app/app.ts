7d3df4debdd7edeadeb269d581cace52158e64ce
Barnabas Matrai <matrai.barnabas1@gmail.com>
2026-02-25 19:21:10 +0100

    create login component

--- tourist-planner/src/app/app.ts
+++ tourist-planner/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Register } from "./components/register/register";
+import { Login } from "./components/login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Register],
+  imports: [RouterOutlet, Register, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
