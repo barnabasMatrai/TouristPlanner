5afb3e6c0a2e0074f0e1c5ff20d751bfd05b5370
Barnabas Matrai <matrai.barnabas1@gmail.com>
2026-02-25 20:47:53 +0100

    implement routing for register and login

--- tourist-planner/src/app/app.routes.ts
+++ tourist-planner/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Register } from './components/register/register';
+import { Login } from './components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+];
