f63d89bdc9184a7fc4ae10f077b48b012bc55789
Jarod <jarodhila77@gmail.com>
2026-03-17 23:19:16 +0100

    Tour_Info

--- tourist-planner/src/app/components/views/dashboard/dashboard.ts
+++ tourist-planner/src/app/components/views/dashboard/dashboard.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
- standalone: true,
+  standalone: true,
   imports: [RouterModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
 
+  constructor(private router: Router) {}
+
+  onLogout() {
+    this.router.navigate(['/home']);
+  }
+
 }
-
-
